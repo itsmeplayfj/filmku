f34a5167bc8e48c813e1b440e7075ac97629b067
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2021-11-18 17:26:33 +0800

    Create gabung.js

---
+++ gabung.js
@@ -0,0 +1,8 @@
+var dataUrl = [
+'//anomalousporch.com/z84tdeyj5?key=cf9108918b929c0e8e6585863921bc00',
+'//anomalousporch.com/fweeqvjv8d?key=d248834943f3e7e44e8f940e6f85f7e2',
+'//anomalousporch.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032',
+'//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
