a6a3ef5b0635bab6563633bb1fc5b6c8b31cf954
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2021-11-22 20:05:54 +0800

    Create pendek.js

---
+++ pendek.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+'//anomalousporch.com/fweeqvjv8d?key=d248834943f3e7e44e8f940e6f85f7e2',
+'//anomalousporch.com/z84tdeyj5?key=cf9108918b929c0e8e6585863921bc00'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
