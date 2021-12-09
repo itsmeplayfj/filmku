014da04b93c530199665c6519a03207a0e547f2f
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2021-12-10 01:15:41 +0800

    Create waduara.js

---
+++ waduara.js
@@ -0,0 +1,5 @@
+var dataUrl = [
+'//www.effectivegatetocontent.com/h8ad994p3g?key=68c6e7dbd635523b65529770d75dbac1'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
