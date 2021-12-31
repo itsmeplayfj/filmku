3f8366ca93e69dda70dfb31a3140988d8ae2bc9d
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-01-01 00:11:33 +0800

    Update cuan.js

--- cuan.js
+++ cuan.js
@@ -2,6 +2,7 @@ var dataUrl = [
 '//anomalousporch.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032',
  'https://anomalousporch.com/fweeqvjv8d?key=d248834943f3e7e44e8f940e6f85f7e2',
  'https://pjjpp.com/mobile_redir.php?section=General&pub=546788&ga=g&desktop=1',
+ 'https://546788.click-allow.top/',
 '//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
  'https://anomalousporch.com/z84tdeyj5?key=cf9108918b929c0e8e6585863921bc00'
 ];
