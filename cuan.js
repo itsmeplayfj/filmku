5e919d2837f8d612c4a676f08da90c1d3c182b27
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-01-11 09:48:41 -0600

    Update cuan.js

--- cuan.js
+++ cuan.js
@@ -1,8 +1,5 @@
 var dataUrl = [
-'https://wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
-'https://wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
-'https://anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
-'https://anomalousporch.com/fweeqvjv8d?key=d248834943f3e7e44e8f940e6f85f7e2'
+'https://anomalousporch.com/e5ntk4vw2?key=afc46c163b5e7aa15937edf1b69b82cc',
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
