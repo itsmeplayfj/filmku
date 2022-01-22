25415dbfc35b12c0306d834ba961761fbac96776
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-01-22 10:54:41 -0600

    Create cuanki.js

---
+++ cuanki.js
@@ -0,0 +1,9 @@
+var dataUrl = [
+'https://wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
+'https://wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
+'https://dev-fivestarmovie.pantheonsite.io/',
+'https://anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
+'https://anomalousporch.com/fweeqvjv8d?key=d248834943f3e7e44e8f940e6f85f7e2'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
