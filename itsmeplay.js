32741264cb21c07652c8fc43f97f452eefa07004
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-01-26 10:50:12 -0600

    Delete itsmeplay.js

--- itsmeplay.js
+++
@@ -1,8 +0,0 @@
-var dataUrl = [
-'https://wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
-'https://bit.ly/3KIFkrM',
-'https://bit.ly/3fUO3ZS',
-'https://bit.ly/3o0VyTE'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
