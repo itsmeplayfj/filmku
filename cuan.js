84478ece29c07282f24be8e3493c64564e121928
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-02-15 01:07:48 -0600

    Update cuan.js

--- cuan.js
+++ cuan.js
@@ -1,5 +1,5 @@
 var dataUrl = [
-'//dev-fivestarmovie.pantheonsite.io/',
+'//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
 '//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
 '//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c'
 ];
