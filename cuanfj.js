80a3eae66ce30d87630ab919770127c86630f8dc
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-02-26 20:45:02 -0600

    Update cuanfj.js

--- cuanfj.js
+++ cuanfj.js
@@ -1,6 +1,7 @@
 var dataUrl = [
 '//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
 '//anomalousporch.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032',
+'//screenmovies3.blogspot.com/'
 '//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
 '//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851'
 ];
