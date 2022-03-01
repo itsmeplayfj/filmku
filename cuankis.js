6fdb05a4b2eaddeb9aca5159e1ea93eec793d106
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-03-01 03:46:07 -0600

    Delete cuankis.js

--- cuankis.js
+++
@@ -1,9 +0,0 @@
-var dataUrl = [
-'https://anomalousporch.com/fweeqvjv8d?key=d248834943f3e7e44e8f940e6f85f7e2',
-'https://wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
-'https://dev-fivestarmovie.pantheonsite.io/',
-'//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
-'//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
