4b8209e8fabd6e141d9727edd2f3a678e46b1868
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-03-01 03:46:09 -0600

    Delete cuan.js

--- cuan.js
+++
@@ -1,7 +0,0 @@
-var dataUrl = [
-'//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
-'//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
-'//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
