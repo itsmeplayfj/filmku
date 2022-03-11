b89c72440f30ec60a0fe1f4dd57efe7d1cfca40d
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-03-10 23:25:57 -0600

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -3,6 +3,7 @@ var dataUrl = [
 '//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
 '//anomalousporch.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032',
 'https://bit.ly/3puU5Wt',
+'https://bit.ly/3GipkKq',
 '//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
 '//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
 '//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac'
