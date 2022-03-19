8f416d32e5765510c56b397e66beebbb19f9344f
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-03-19 01:47:51 -0700

    Update currencyfinance.js

--- currencyfinance.js
+++ currencyfinance.js
@@ -1,5 +1,5 @@
 src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
-	$(document).ready(function()
+	<script> $(document).ready(function()
 	{
 	  $('body').addClass('xepo_ads');
 	});
