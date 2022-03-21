38351f5fa3f9a2b9f5c96e609987e34e05b3e7c3
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-03-21 02:48:30 -0700

    Update and rename boxofficefilm.js to boxofficefilms.js

--- boxofficefilms.js
+++ boxofficefilms.js
@@ -1,6 +1,6 @@
 var dataUrl = [
 '//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c',
-'//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c'
+'//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c',
 '//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
 '//anomalousporch.com/e5ntk4vw2?key=afc46c163b5e7aa15937edf1b69b82cc',
 '//anomalousporch.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032',
