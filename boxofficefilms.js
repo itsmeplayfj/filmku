32417f19423b7f18a89ef279cd503c61be8ebf55
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-04-07 23:27:20 -0700

    Update boxofficefilms.js

--- boxofficefilms.js
+++ boxofficefilms.js
@@ -1,12 +1,18 @@
 var dataUrl = [
+'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
 'https://bit.ly/3hZfPFz',
 '//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c',
+'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
 '//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c',
+'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
 '//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
+'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
 '//anomalousporch.com/e5ntk4vw2?key=afc46c163b5e7aa15937edf1b69b82cc',
+'//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
 '//anomalousporch.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032',
-'//anomalousporch.com/fweeqvjv8d?key=d248834943f3e7e44e8f940e6f85f7e2',
-'https://bit.ly/3puU5Wt'
+'//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
+'https://bit.ly/3puU5Wt',
+'//anomalousporch.com/fweeqvjv8d?key=d248834943f3e7e44e8f940e6f85f7e2'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
