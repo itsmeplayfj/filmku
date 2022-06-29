bf510f86c5f4b9539b5b0e561d9cf020b9b43de0
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-06-29 11:02:12 -0700

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -1,12 +1,13 @@
 var dataUrl = [
-'//anomalousporch.com/s12ygwr4j?key=0c236aab12a231150e5db3f8b366197c',
-'//anomalousporch.com/hh9m6iq8u?key=b1b4a076f4bf1c1aa140cc71754f0d67',
-'//anomalousporch.com/bcf9j7den?key=6f0346ec565f3485f607d1f027ca2032',
-'https://bit.ly/3puU5Wt',
-'https://bit.ly/3GipkKq',
-'//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
+'https://wavermerchandiseweird.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
+'//cryptominingdoge.blogspot.com/',
+'//cryptominingfilm.blogspot.com/',
+'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
 '//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
-'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac'
+'//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
+'//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
+'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
+'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
