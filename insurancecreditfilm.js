6cc50f3e606046a65b23f1f54aca9d66049ececd
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-07-04 23:54:39 -0700

    Update insurancecreditfilm.js

--- insurancecreditfilm.js
+++ insurancecreditfilm.js
@@ -1,13 +1,13 @@
 var dataUrl = [
+'//cryptominingdoge.blogspot.com/',
 '//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
 '//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
 '//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
 '//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
 '//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
 '//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
-'//cryptominingfilm.blogspot.com/',
-'//cryptominingdoge.blogspot.com/',
-  'https://wavermerchandiseweird.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9'
+'https://wavermerchandiseweird.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
+'//cryptominingfilm.blogspot.com/'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
