20772ecea4114c1d6fa43f6434127ee6fbf1030d
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-10-25 18:38:54 -0700

    Update itsmeplays.js

--- itsmeplays.js
+++ itsmeplays.js
@@ -1,5 +1,7 @@
 var dataUrl = [
 'https://anomalousporch.com/xtqed6ym?key=96fc440cd6ee5c86fdc6824e763d6d0f',
+'https://wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
+'//wavermerchandiseweird.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
 'https://anomalousporch.com/gw4jagcm?key=40440900b0477fd9b7dbfcc4cf84ac19'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
