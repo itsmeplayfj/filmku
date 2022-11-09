63cec87abfd2c40c042c97c4d169953544523a4a
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2022-11-09 04:14:28 -0700

    Update insurancecreditfilm.js

--- insurancecreditfilm.js
+++ insurancecreditfilm.js
@@ -1,9 +1,9 @@
 var dataUrl = [
-'//cryptominingdoge.blogspot.com/2022/05/streaming-download-online-film.html/',
 '//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
 '//wavermerchandiseweird.com/k78nwxfv?key=e668079d92f5c385e97d9e9f30beedd3',
 '//wavermerchandiseweird.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
 '//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
+'//screenmovies44.blogspot.com/',
 '//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
 '//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851'
 ];
