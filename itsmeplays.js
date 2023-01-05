a6867918b6c401cac4bbfe70faaea44cc6524318
itsmeplayfj <94605694+itsmeplayfj@users.noreply.github.com>
2023-01-05 01:20:51 -0700

    Update itsmeplays.js

--- itsmeplays.js
+++ itsmeplays.js
@@ -1,8 +1,11 @@
 var dataUrl = [
-'https://anomalousporch.com/xtqed6ym?key=96fc440cd6ee5c86fdc6824e763d6d0f',
-'https://wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac',
+'//wavermerchandiseweird.com/z0sfd1pts?key=2431d39c120107799ee55bcab97fc536',
+'//wavermerchandiseweird.com/idt1y6nan?key=ce537f67a6c57e27e292db0da8179851',
+'//wavermerchandiseweird.com/k78nwxfv?key=e668079d92f5c385e97d9e9f30beedd3',
+ '//cryptominingdoge.blogspot.com/',
 '//wavermerchandiseweird.com/rmdh7vzvs?key=ed9adfc3b8491179ad53c3e027d8b8a9',
-'https://anomalousporch.com/gw4jagcm?key=40440900b0477fd9b7dbfcc4cf84ac19'
+'//wavermerchandiseweird.com/qqqysyvbg?key=1d3303b4d7decb61514e9d25a740edfe',
+'//wavermerchandiseweird.com/tuf9tdkp?key=6729f99d594d33885cec1eb3981e92ac'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
